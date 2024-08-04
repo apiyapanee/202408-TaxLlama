ec79eb7cd2bd1b0cce7ac7b5d828c4d089cc596f
apiyapan <apiyapan@caltech.edu>
2024-08-04 10:38:24 -0700

    llama parse uploaded file

--- backend/file_upload.py
+++ backend/file_upload.py
@@ -1,10 +1,40 @@
 from flask import Flask, request
 from flask_cors import CORS
 import os
-import PyPDF2
+from dotenv import load_dotenv
+load_dotenv()
+
+from llama_parse import LlamaParse
+from llama_index.core import SimpleDirectoryReader
+from llama_index.core import VectorStoreIndex
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+
+
+api_key = os.getenv("LLAMA_CLOUD_API_KEY")
+
+# Example usage of the API key
+print("Your API key is:", api_key)
+
+def parse_doc(file, queries, filetype=".pdf", result_type="markdown"):
+    parser = LlamaParse(
+        result_type=result_type
+    )
+    documents = SimpleDirectoryReader(input_files=[file], file_extractor={filetype: parser}).load_data()
+    index = VectorStoreIndex.from_documents(documents)
+    query_engine = index.as_query_engine()
+    print("Llama parse done")
+    responses = []
+    for q in queries:
+        responses.append(ask_doc(q, query_engine))
+    print("responses: ", responses)
+    return responses, query_engine
+
+def ask_doc(query, query_engine):
+    response = query_engine.query(query)
+    return response
+
 
 @app.route('/')
 def home():
